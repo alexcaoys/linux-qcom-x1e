tobhe/linux

Add qcom-x1e flavor
Signed-off-by: Tobias Heider <tobias.heider@canonical.com>

debian.qcom-x1e/rules.d/arm64.mk
@@ -1,0 +1,19 @@
+build_arch	= arm64
+defconfig	= defconfig
+flavours	= qcom-x1e
+build_image	= Image.gz
+kernel_file	= arch/$(build_arch)/boot/Image.gz
+install_file	= vmlinuz
+no_dumpfile	= true
+
+vdso		= vdso_install
+
+do_tools_usbip  = true
+do_tools_cpupower = true
+do_tools_perf   = true
+do_tools_perf_jvmti = true
+do_tools_perf_python = true
+do_tools_bpftool = true
+do_tools_rtla = true
+
+do_dtbs		= true

alexcaoys/linux-qcom-x1e

debian.qcom-x1e/rules.d/arm64.mk
@@ -1,0 +1,19 @@
+build_arch	= arm64
+defconfig	= defconfig
+flavours	= qcom-x1e
+build_image	= vmlinuz.efi
+kernel_file	= arch/$(build_arch)/boot/vmlinuz.efi
+install_file	= vmlinuz
+no_dumpfile	= true
+
+vdso		= vdso_install
+
+do_tools_usbip  = true
+do_tools_cpupower = true
+do_tools_perf   = true
+do_tools_perf_jvmti = true
+do_tools_perf_python = true
+do_tools_bpftool = true
+do_tools_rtla = true
+
+do_dtbs		= true